b50ec78c13181abe340c4ef53870f1c6618233ee
Gajus Kuizinas <gajus@gajus.com>
2016-11-14 20:07:58 +0000

    fix: make delay resolve value explicit

--- src/delay.js
+++ src/delay.js
@@ -1,13 +1,15 @@
 // @flow
 
-type DelayType = (ms: number) => Promise<any>;
+type DelayType = (ms: number) => Promise<void>;
 
 /**
  * Creates a promise that is scheduled to resolve after a set delay.
  */
 const delay: DelayType = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
+    setTimeout(() => {
+      resolve();
+    }, ms);
   });
 };
 
