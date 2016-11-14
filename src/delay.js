f12c8cb427800501f3214f8328f87003430240e7
Gajus Kuizinas <gajus@gajus.com>
2016-11-14 20:07:03 +0000

    docs: document delay

--- src/delay.js
+++ src/delay.js
@@ -2,6 +2,9 @@
 
 type DelayType = (ms: number) => Promise<any>;
 
+/**
+ * Creates a promise that is scheduled to resolve after a set delay.
+ */
 const delay: DelayType = (ms) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
